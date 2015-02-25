6cf4021dbead28ea8dff298040725d6117abf3cd
David Parker <David.theo.parker@gmail.com>
2015-02-25 15:50:04 +0200

    add hopeful message

--- Tut1/hello.cpp
+++ Tut1/hello.cpp
@@ -1,9 +1,9 @@
 // John Poole, CM3, 25/02/2015
 // My first C++ program (Hello, World!)
 #include <iostream>
-
 using namespace std;
 
 int main() {
 	cout << "Hello, World!" << endl;
+	cout << "I hope this works" << endl;
 }
